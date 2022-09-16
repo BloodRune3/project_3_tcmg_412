cc8a226e70e9d117b0f0f705d0103942630e75bd
liz728 <112724093+liz728@users.noreply.github.com>
2022-09-15 22:14:54 -0500

    Added Code for total Requests last 6 months
    Please review code

--- project_3.py
+++ project_3.py
@@ -15,7 +15,34 @@ else:
     print("there was already a log on your local machine")
 
 # how many total requests have been made in the last 6 months, output to txt file
+with open("project_3_output.txt") as file_in:
+    array = []
+    for line in file_in:
+        array.append(line)
+        
+count = 0
 
+for i in array:
+    if i.find("Apr") != -1 and i.find("1995") != -1 and i.find("[11") != -1:
+        count += 1
+    elif i.find("May") != -1 and i.find("1995") != -1:
+        count += 1
+    elif i.find("Jun") != -1 and i.find("1995") != -1:
+        count += 1
+    elif i.find("Jul") != -1 and i.find("1995") != -1:
+        count += 1
+    elif i.find("Aug") != -1 and i.find("1995") != -1:
+        count += 1
+    elif i.find("Sep") != -1 and i.find("1995") != -1:
+        count += 1
+        array[count] = i
+    elif i.find("Oct") != -1 and i.find("1995") != -1:
+        count += 1
+
+countall = len(array)
+
+print("Total Number of Requests in the past six months: ", count)
+print("Total Number of Requests: ", countall)
 
 # how many total requests have been made over the entire report, output to txt file
 
